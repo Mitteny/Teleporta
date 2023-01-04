cb5f04d250639d74286b2e6072fd10db4d9aa763
SuperShjibi <shjibi@outlook.com>
2023-01-04 19:27:47 +0800

    修复了有时会出现空指针异常的错误

--- src/main/java/top/shjibi/teleporta/commands/tpa/TeleportRequest.java
+++ src/main/java/top/shjibi/teleporta/commands/tpa/TeleportRequest.java
@@ -5,22 +5,16 @@ import org.bukkit.entity.Player;
 
 import static top.shjibi.plugineer.util.StringUtil.color;
 
-/** 代表了一个传送请求,包含了起始地(Player),目的地(Player),请求时间(long),以及类型(TeleportType) */
+/**
+ * 代表了一个传送请求,包含了起始地(Player),目的地(Player),请求时间(long),以及类型(TeleportType)
+ */
 public record TeleportRequest(String from, String to, long start, TeleportType type) {
 
     public static final int REMOVE_DELAY = 60;
 
-    public Player getFrom() {
-        return Bukkit.getPlayerExact(this.from);
-    }
-
-    public Player getTo() {
-        return Bukkit.getPlayerExact(this.to);
-    }
-
     public boolean accept() {
-        Player to = getTo();
-        Player from = getFrom();
+        Player to = Bukkit.getPlayerExact(this.to);
+        Player from = Bukkit.getPlayer(this.from);
         if (from == null || to == null) return false;
         if (shouldRemove()) return false;
         from.teleport(to.getLocation());
@@ -33,10 +27,12 @@ public record TeleportRequest(String from, String to, long start, TeleportType t
 
     public void sendRemoveMessage() {
         boolean typeBool = (type == TeleportType.THERE);
-        Player reqSender = typeBool ? getFrom() : getTo();
-        Player receiver = typeBool ? getTo() : getFrom();
-        reqSender.sendMessage(color("&7你发送给&e" + receiver.getName() + "&7的请求已过期!"));
-        receiver.sendMessage(color("&e" + reqSender.getName() + "&7发送你的请求已过期!"));
+        String senderName = typeBool ? from : to;
+        String receiverName = typeBool ? to : from;
+        Player reqSender = Bukkit.getPlayerExact(senderName);
+        Player receiver = Bukkit.getPlayerExact(receiverName);
+        if (reqSender != null) reqSender.sendMessage(color("&7你发送给&e" + receiverName + "&7的请求已过期!"));
+        if (receiver != null) receiver.sendMessage(color("&e" + senderName + "&7发送你的请求已过期!"));
     }
 
     @Override
